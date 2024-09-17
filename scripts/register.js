befc86c8105e1a16470123a3b578445768fd8fb6
MaxKessie <abcbeautyhub@gmail.com>
2024-09-17 15:56:39 +0000

    JavaScript variables update

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,28 @@
 // Get form element by id
-const registerForm = document.getElementById('register');
+const registerForm = document.querySelector('#register');
+console.log(registerForm);
 // Handle form submit event
-registerForm.onsubmit = function(event) {
+registerForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log(event);
-}
+    // collect form data
+    const formData = new FormData(registerForm);
+   // Send Data to backend
+   // Display success message to user
+   const messageH1= document.querySelector('#message');
+   messageH1.textContent = 'Account registered successfully!';
+    });
+
+//registerForm.onsubmit = function(event) {
+ //   event.preventDefault();
+ //   console.log(event);
+
+ //declare variable age
+ // const age = 71;
+
+ //define square age function
+ function squareAge(age) {
+    return age ** 2;
+ }
+
+ const squaredAge = squareAge(34);
+ console.log(squaredAge);
